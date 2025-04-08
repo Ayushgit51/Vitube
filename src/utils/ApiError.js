56822082119b26f047866cc3d12ec4fe6981e336
Ayush Kumar <114606958+Ayushgit51@users.noreply.github.com>
2025-04-08 16:05:59 +0530

    util Created

---
+++ src/utils/ApiError.js
@@ -0,0 +1,19 @@
+class ApiError extends Error {
+  constructor(message= "Something went wrong", statusCode, errors=[], stack="") {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if(stack) {
+      this.stack = stack;
+    }else{
+        Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
+// This class extends the built-in Error class in JavaScript.
