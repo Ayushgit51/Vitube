4b87a77e7f7a478782ccba7bbd245e40a141290f
Ayush Kumar <114606958+Ayushgit51@users.noreply.github.com>
2025-04-29 14:28:19 +0530

    add all routes of user controller in router

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from 'express';
-import { logoutUser, loginUser, registerUser, refreshAccessToken } from '../controllers/user.controller.js';
+import { 
+    logoutUser, 
+    loginUser, 
+    registerUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateAvatar, 
+    updateCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory 
+} from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -8,7 +20,7 @@ const router = Router();
 router.route("/register").post(upload.fields(// middleware for handling multipart/form-data)
     // fields for avatar and coverImage
     [
-        { name: "avatar", maxCount: 1 }, 
+        { name: "avatar", maxCount: 1 },
         { name: "coverImage", maxCount: 1 }
     ]),
     registerUser
@@ -20,5 +32,20 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar)
+
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/history").get(verifyJWT, getWatchHistory)
+
 
 export default router;
